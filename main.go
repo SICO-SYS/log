7d7293a666469181aab9af98bbea1a2f7b30f78a
SingleCorner <sinerwr@gmail.com>
2017-04-18 18:03:34 +0800

    define_error_msg

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ package log
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/SiCo-DevOps/cfg"
 )
@@ -23,7 +24,71 @@ var (
 	logFileDir  = cfg.Config.Log.Logpath
 )
 
-func WriteLog(level string, msg string) {
+func LogMsg(s int8, fn string) {
+	msg := "[unknown] "
+	switch s {
+	case 0:
+		msg += "Success"
+	default:
+		msg += "Undefine"
+
+	}
+	LogProduce("unknown", time.Now().Format("2006-01-02 15:04:05")+" "+fn+" "+msg)
+}
+
+func LogWarnMsg(s int8, fn string) {
+	msg := "[warining] "
+	switch s {
+	case 0:
+		msg += "Success"
+	default:
+		msg += "Undefine"
+
+	}
+	LogProduce("warning", time.Now().Format("2006-01-02 15:04:05")+" "+fn+" "+msg)
+}
+
+func LogErrMsg(s int8, fn string) {
+	msg := "[Error] "
+	switch s {
+	case 0:
+		msg += "Cannot open redis connect"
+	case 1:
+		msg += "Cannot open mongo connect"
+	case 2:
+		msg += "Cannot open mysql connect"
+	case 3:
+		msg += "Cannot open mq connect"
+	case 4:
+		msg += "Cannot open grpc connect"
+	case 10:
+		msg += "Redis operation error"
+	case 11:
+		msg += "Redis unknown error"
+	case 12:
+		msg += "Redis value parse error"
+	default:
+		msg = "Undefine"
+
+	}
+	LogProduce("error", time.Now().Format("2006-01-02 15:04:05")+" "+fn+" "+msg)
+}
+
+// DO NOT USE THIS FUNC
+func LogFatalMsg(s int8, fn string) {
+	msg := "[fatal] "
+	switch s {
+	case 0:
+		msg += "Cannot open redis connect"
+	default:
+		msg = "Undefine"
+
+	}
+	LogProduce("fatal", time.Now().Format("2006-01-02 15:04:05")+" "+fn+" "+)
+	os.Exit(1)
+}
+
+func LogProduce(level string, msg string) {
 	defer func() {
 		if err := recover(); err != nil {
 			log.Println("File cannot Write in " + filename)
